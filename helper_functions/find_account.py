fbb58d6606be7fd7a56dddb58681a9fd853dc571
Marin Maulbon <marin.maulbon@gmail.com>
2025-11-14 16:47:30 +0100

    forgot something in url

--- helper_functions/find_account.py
+++ helper_functions/find_account.py
@@ -4,7 +4,7 @@ def find_account(url, api_key):
     account_name = input("Enter your account name: ")
     correct_account_name = account_name.replace(" ", "%20")
     account_tag = input("Enter your account tag (e.g. EUW): ")
-    account_url = url + f"account/v1/accounts/by-riot-id/{correct_account_name}/{account_tag}?api_key={api_key}"
+    account_url = url + f"riot/account/v1/accounts/by-riot-id/{correct_account_name}/{account_tag}?api_key={api_key}"
     resp = requests.get(account_url)
     puuid = resp.json()["puuid"]
     print(puuid)
