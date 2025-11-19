6d878fc34473b3fb28c12c83969cacdcc6573c12
Marin Maulbon <marin.maulbon@gmail.com>
2025-11-19 12:32:17 +0100

    better output for the placements

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import requests
 from variables import API_KEY, URL
 from helper_functions.last_match_content import last_match_content
 
-match_content = last_match_content(URL, API_KEY)
-for participant in match_content["info"]["participants"]:
-    name = participant["riotIdGameName"]
-    placement = participant["placement"]
-    print(f"{name} placed {placement} in your last match.")
+
+def main():
+    match_content = last_match_content(URL, API_KEY)
+    final_result = [None, None, None, None, None, None, None, None]
+    for participant in match_content["info"]["participants"]:
+        name = participant["riotIdGameName"]
+        placement = participant["placement"]
+        final_result[placement - 1] = name
+
+    for i in range(len(final_result)):
+        print(f"#{i + 1} {final_result[i]}")
+
+main()
